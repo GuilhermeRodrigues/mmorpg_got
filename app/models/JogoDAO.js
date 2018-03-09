dfa8d9307eb211e517d332efce7e1b07d4bfcdc7
GuilhermeRodrigues <guilherme17_rodrigues@hotmail.com>
2018-03-09 17:25:09 -0300

    Atualizando moedas
    Atualizando moedas.

--- app/models/JogoDAO.js
+++ app/models/JogoDAO.js
@@ -54,8 +54,28 @@ JogoDAO.prototype.acao = function(acao){
 
             collection.insert(acao);
             
+            
+        });
+
+        mongoclient.collection("jogo", function(err, collection){
+            
+            var moedas = null;
+
+            switch(parseInt(acao.acao)){
+                case 1: moedas = -2 * acao.quantidade; break;
+                case 2: moedas = -3 * acao.quantidade; break;
+                case 3: moedas = -1 * acao.quantidade; break;
+                case 4: moedas = -1 * acao.quantidade; break;
+            }
+
+            collection.update(
+                {usuario : acao.usuario},
+                {$inc : {moeda : moedas}}
+            );
+
             mongoclient.close();
         });
+
     });
 }
 
